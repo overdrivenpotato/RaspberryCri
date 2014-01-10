90c7cc681dc2f58c2f279a5eeed4aeed28c9b93c
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-01-09 23:38:27 -0500

    Added program.

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <sys/time.h>
+#include <fstream>
+#include <cstdlib>
+#include <string.h>
+using namespace std;
+
+class Timer
+{
+    timeval timer[2];
+
+public:
+
+    timeval start()
+    {
+        gettimeofday(&this->timer[0], NULL);
+        return this->timer[0];
+    }
+
+    timeval stop()
+    {
+        gettimeofday(&this->timer[1], NULL);
+        return this->timer[1];
+    }
+
+    int duration() const
+    {
+        int secs(this->timer[1].tv_sec - this->timer[0].tv_sec);
+        int usecs(this->timer[1].tv_usec - this->timer[0].tv_usec);
+
+        if(usecs < 0)
+        {
+            --secs;
+            usecs += 1000000;
+        }
+
+        return static_cast<int>(secs * 1000 + usecs / 1000.0 + 0.5);
+    }
+};
+
+int main()
+{
+    string path = strcat(getenv("HOME"), "/Benchmark.txt");
+    cout << "Using file " << path << "\n";
+
+
+    ofstream file;
+    file.open(path.c_str(), ios::out | ios::app);
+
+    if(!file.is_open()){
+        cout << "Opening in trunc mode...\n";
+        file.open(path.c_str(), ios::in | ios::out | ios::trunc);
+        if(!file.is_open()){
+            cout << "Encountered an error opening the file.\n";
+            return -1;
+        }
+    }
+
+    Timer timer;
+    for(int i = 0; i < 400; i++){
+        timer.start();
+        string str;
+        for(int i = 0; i < 30000000; i++) {
+            str += i;
+        }
+        timer.stop();
+
+        string w;
+        w << "Took: " << timer.duration() << "ms\n";
+        cout << w;
+        file << w;
+
+        file.flush();
+    }
+    file.close();
+
+    return 0;
+}
